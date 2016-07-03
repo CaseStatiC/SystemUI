eab1e1fa3d594932d1487bea5d9cc199ed4ef7f9
xuwanjin <xuwanjin1991@gmail.com>
2016-07-03 22:57:32 +0800

    add KeyguardService log

--- app/src/main/java/com/android/systemui/keyguard/KeyguardService.java
+++ app/src/main/java/com/android/systemui/keyguard/KeyguardService.java
@@ -41,6 +41,7 @@ public class KeyguardService extends Service {
 
     @Override
     public void onCreate() {
+        Log.d(TAG, "onCreate: ");
         ((SystemUIApplication) getApplication()).startServicesIfNeeded();
         mKeyguardViewMediator =
                 ((SystemUIApplication) getApplication()).getComponent(KeyguardViewMediator.class);
@@ -48,10 +49,12 @@ public class KeyguardService extends Service {
 
     @Override
     public IBinder onBind(Intent intent) {
+        Log.d(TAG, "onBind: ");
         return mBinder;
     }
 
     void checkPermission() {
+        Log.d(TAG, "checkPermission: ");
         // Avoid deadlock by avoiding calling back into the system process.
         if (Binder.getCallingUid() == Process.SYSTEM_UID) return;
 
@@ -67,18 +70,21 @@ public class KeyguardService extends Service {
 
         @Override // Binder interface
         public void addStateMonitorCallback(IKeyguardStateCallback callback) {
+            Log.d(TAG, "mBinder: addStateMonitorCallback: ");
             checkPermission();
             mKeyguardViewMediator.addStateMonitorCallback(callback);
         }
 
         @Override // Binder interface
         public void verifyUnlock(IKeyguardExitCallback callback) {
+            Log.d(TAG, "mBinder: verifyUnlock: ");
             checkPermission();
             mKeyguardViewMediator.verifyUnlock(callback);
         }
 
         @Override // Binder interface
         public void keyguardDone(boolean authenticated, boolean wakeup) {
+            Log.d(TAG, "mBinder: keyguardDone: ");
             checkPermission();
             // TODO: Remove wakeup
             mKeyguardViewMediator.keyguardDone(authenticated);
@@ -86,102 +92,119 @@ public class KeyguardService extends Service {
 
         @Override // Binder interface
         public void setOccluded(boolean isOccluded) {
+            Log.d(TAG, "mBinder: setOccluded: ");
             checkPermission();
             mKeyguardViewMediator.setOccluded(isOccluded);
         }
 
         @Override // Binder interface
         public void dismiss() {
+            Log.d(TAG, "mBinder: dismiss: ");
             checkPermission();
             mKeyguardViewMediator.dismiss();
         }
 
         @Override // Binder interface
         public void onDreamingStarted() {
+            Log.d(TAG, "mBinder: onDreamingStarted: ");
             checkPermission();
             mKeyguardViewMediator.onDreamingStarted();
         }
 
         @Override // Binder interface
         public void onDreamingStopped() {
+            Log.d(TAG, "mBinder: onDreamingStopped: ");
             checkPermission();
             mKeyguardViewMediator.onDreamingStopped();
         }
 
         @Override // Binder interface
         public void onStartedGoingToSleep(int reason) {
+            Log.d(TAG, "mBinder: onStartedGoingToSleep: ");
             checkPermission();
             mKeyguardViewMediator.onStartedGoingToSleep(reason);
         }
 
         @Override // Binder interface
         public void onFinishedGoingToSleep(int reason) {
+            Log.d(TAG, "mBinder: onFinishedGoingToSleep: ");
             checkPermission();
             mKeyguardViewMediator.onFinishedGoingToSleep(reason);
         }
 
         @Override // Binder interface
         public void onStartedWakingUp() {
+            Log.d(TAG, "mBinder: onStartedWakingUp: ");
             checkPermission();
             mKeyguardViewMediator.onStartedWakingUp();
         }
 
         @Override // Binder interface
         public void onScreenTurningOn(IKeyguardDrawnCallback callback) {
+            Log.d(TAG, "mBinder: onScreenTurningOn: ");
             checkPermission();
             mKeyguardViewMediator.onScreenTurningOn(callback);
         }
 
         @Override // Binder interface
         public void onScreenTurnedOn() {
+            Log.d(TAG, "mBinder: onScreenTurnedOn: ");
             checkPermission();
             mKeyguardViewMediator.onScreenTurnedOn();
         }
 
         @Override // Binder interface
         public void onScreenTurnedOff() {
+            Log.d(TAG, "mBinder: onScreenTurnedOff: ");
             checkPermission();
             mKeyguardViewMediator.onScreenTurnedOff();
         }
 
         @Override // Binder interface
         public void setKeyguardEnabled(boolean enabled) {
+            Log.d(TAG, "mBinder: setKeyguardEnabled: ");
             checkPermission();
             mKeyguardViewMediator.setKeyguardEnabled(enabled);
         }
 
         @Override // Binder interface
         public void onSystemReady() {
+            Log.d(TAG, "mBinder: onSystemReady: ");
             checkPermission();
             mKeyguardViewMediator.onSystemReady();
         }
 
         @Override // Binder interface
         public void doKeyguardTimeout(Bundle options) {
+            Log.d(TAG, "mBinder: doKeyguardTimeout: ");
             checkPermission();
             mKeyguardViewMediator.doKeyguardTimeout(options);
         }
 
         @Override // Binder interface
         public void setCurrentUser(int userId) {
+            Log.d(TAG, "mBinder: setCurrentUser: ");
             checkPermission();
             mKeyguardViewMediator.setCurrentUser(userId);
         }
 
         @Override
         public void onBootCompleted() {
+            Log.d(TAG, "mBinder: onBootCompleted: ");
             checkPermission();
             mKeyguardViewMediator.onBootCompleted();
         }
 
         @Override
         public void startKeyguardExitAnimation(long startTime, long fadeoutDuration) {
+            Log.d(TAG, "mBinder: startKeyguardExitAnimation: ");
             checkPermission();
             mKeyguardViewMediator.startKeyguardExitAnimation(startTime, fadeoutDuration);
         }
 
         @Override
         public void onActivityDrawn() {
+            Log.d(TAG, "mBinder: onActivityDrawn: ");
             checkPermission();
             mKeyguardViewMediator.onActivityDrawn();
         }
